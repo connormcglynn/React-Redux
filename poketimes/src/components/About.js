aec71f517932f8ec7fde1dcfdb73465e5c5a2a28
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-09-12 11:56:12 -0400

    finished lessons 25-29 and react router, axios, etc

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow'
+
+const About = () => {
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque, vel hic. Deserunt sapiente, quae nesciunt hic eum natus unde dolorem amet, maiores animi optio impedit voluptatibus obcaecati magni cumque quisquam.</p>
+        </div>
+    )
+}
+
+export default Rainbow(About)
---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque, vel hic. Deserunt sapiente, quae nesciunt hic eum natus unde dolorem amet, maiores animi optio impedit voluptatibus obcaecati magni cumque quisquam.</p>
+        </div>
+    )
+}
+
+export default Contact
